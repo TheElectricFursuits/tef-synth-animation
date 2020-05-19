46a2fa8bbb734306be24a2a5456fea2a5750691e
Xasin <davidbailey.2889@gmail.com>
2020-05-19 20:40:50 +0200

    Move Ruby code to more appropriate location

---
+++ Ruby/lib/tef/animation.rb
@@ -0,0 +1,11 @@
+
+require_relative 'Animation/Animation_Handler.rb'
+require_relative 'Animation/Eyes.rb'
+
+require_relative 'ParameterStack/Stack.rb'
+
+require_relative 'ProgramSelection/ProgramSelector.rb'
+require_relative 'ProgramSelection/SequenceCollection.rb'
+require_relative 'ProgramSelection/SoundCollection.rb'
+
+require_relative 'Sequencing/SequencePlayer.rb'
